d332263bcc8377b08f7fab3bca52d1d4d03384d6
Hans Dieter Pearcey <hdp@pobox.com>
2008-01-11 12:28:49 -0500

    get files from BackPAN

---
+++ lib/XPAN/Injector/BackPAN.pm
@@ -0,0 +1,37 @@
+use strict;
+use warnings;
+
+package XPAN::Injector::BackPAN;
+
+use base qw(XPAN::Injector::Mech);
+use Parse::BACKPAN::Packages;
+use CPAN::DistnameInfo;
+
+my $p;
+
+sub scheme { 'backpan' }
+
+sub scrape {
+  my ($self, $name) = @_;
+
+  $p ||= Parse::BACKPAN::Packages->new;
+
+  # fake it!
+  my $info = CPAN::DistnameInfo->new("$name.tar.gz");
+
+  my (@d) = grep {
+    $_->dist eq $info->dist and (
+      defined $info->version and (
+        $_->version eq $info->version
+      )
+    )
+  } $p->distributions($info->dist);
+
+  unless (@d) {
+    die "no distributions found matching '$name'";
+  }
+
+  return "http://backpan.cpan.org/" . $d[-1]->prefix;
+}
+
+1;
