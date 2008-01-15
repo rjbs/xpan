6a8b46fd82c663aa7a5b8750af7cfdc2bc757962
Hans Dieter Pearcey <hdp@pobox.com>
2008-01-15 13:56:01 -0500

    tidy up

--- lib/XPAN/Dist.pm
+++ lib/XPAN/Dist.pm
@@ -24,23 +24,25 @@ __PACKAGE__->meta->setup(
   columns => [
     id       => { type => 'integer', not_null => 1 },
     name     => { type => 'varchar', length   => 100, not_null => 1 },
-    file     => { type => 'varchar', length   => 110, not_null => 1 },
     version  => { type => 'varchar', length   => 20 },
+    file     => { type => 'varchar', length   => 110, not_null => 1 },
     abstract => { type => 'text' },
   ],
 
   primary_key_columns => ['id'],
 
+  unique_keys => [ ['name', 'version' ] ],
+
   relationships => [
     modules => {
-      type => 'one to many',
-      class => 'XPAN::Module',
+      type       => 'one to many',
+      class      => 'XPAN::Module',
       column_map => { id => 'dist_id' },
     },
 
     dependencies => {
-      type => 'one to many',
-      class => 'XPAN::Dependency',
+      type       => 'one to many',
+      class      => 'XPAN::Dependency',
       column_map => { id => 'dist_id' },
     },
   ],
