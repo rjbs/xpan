6bfe5d40c640a511feabce49bc5ff42bdde12f08
Hans Dieter Pearcey <hdp@pobox.com>
2008-01-23 15:08:52 -0500

    clean up boilerplate

--- lib/XPAN.pm
+++ lib/XPAN.pm
@@ -1,52 +1,21 @@
-package XPAN;
-
-use warnings;
 use strict;
+use warnings;
+
+package XPAN;
 
 =head1 NAME
 
-XPAN - The great new XPAN!
+XPAN - X Perl Archive Network
 
 =head1 VERSION
 
-Version 0.01
+Version 0.001
 
 =cut
 
-our $VERSION = '0.01';
-
+our $VERSION = '0.001';
 
 =head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use XPAN;
-
-    my $foo = XPAN->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
 
 =head1 AUTHOR
 
@@ -57,9 +26,6 @@ Hans Dieter Pearcey, C<< <hdp at cpan.org> >>
 Please report any bugs or feature requests to C<bug-xpan at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=XPAN>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
-
-
 
 =head1 SUPPORT
 
