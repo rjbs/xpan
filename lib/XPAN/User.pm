d5b9121e44718fab702194bc7e875693043c053b
Hans Dieter Pearcey <hdp@pobox.com>
2008-03-25 14:52:40 -0400

    add authority

--- lib/XPAN/User.pm
+++ lib/XPAN/User.pm
@@ -5,4 +5,13 @@ package XPAN::User;
 
 use Moose;
 
+use Sys::Hostname::Long;
+
+has authority => (
+  is => 'ro',
+  isa => 'Str',
+  lazy => 1,
+  default => sub { sprintf '%s@%s', scalar(getpwuid($<)), hostname_long },
+);
+
 1;
