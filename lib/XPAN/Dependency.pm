05f5cd9535cbdf9c52e7ed987f35dae8382fcef3
Hans Dieter Pearcey <hdp@pobox.com>
2008-03-26 10:31:03 -0400

    make version and source longer (why not?)

--- lib/XPAN/Dependency.pm
+++ lib/XPAN/Dependency.pm
@@ -11,8 +11,8 @@ CREATE TABLE dependencies (
   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   dist_id INTEGER NOT NULL,
   name VARCHAR(100) NOT NULL,
-  version VARCHAR(20) NOT NULL,
-  source VARCHAR(20) NOT NULL,
+  version VARCHAR(40) NOT NULL,
+  source VARCHAR(40) NOT NULL,
   UNIQUE(dist_id, name)
 );
 END
@@ -25,8 +25,8 @@ __PACKAGE__->meta->setup(
     id      => { type => 'integer', not_null => 1 },
     dist_id => { type => 'integer', not_null => 1 },
     name    => { type => 'varchar', length   => 100, not_null => 1 },
-    version => { type => 'varchar', length   => 20, not_null => 1 },
-    source  => { type => 'varchar', length   => 20, not_null => 1 },
+    version => { type => 'varchar', length   => 40, not_null => 1 },
+    source  => { type => 'varchar', length   => 40, not_null => 1 },
   ],
 
   primary_key_columns => ['id'],
