666637ff9d0d6ca16aa4676c68cc7ecae59a3d65
Hans Dieter Pearcey <hdp@pobox.com>
2008-03-27 11:42:42 -0400

    switch author/ to id/; unify path part handling

--- lib/URI/cpan.pm
+++ lib/URI/cpan.pm
@@ -7,46 +7,21 @@ use Carp ();
 
 sub _init {
   my $self = shift->SUPER::_init(@_);
+  my $class = ref($self);
   my $auth = $self->authority;
   unless ($auth) {
-    Carp::croak "invalid URI::cpan: missing authority (in $self)";
+    Carp::croak "invalid $class: missing authority (in $self)";
   }
 
-  my $class = "URI::cpan::$auth";
-  if ($auth =~ /^[A-Z]+$/) {
-    $class = "URI::cpan::author";
-  }
+  my $subclass = "$class\::$auth";
   {
     no strict 'refs';
-    if (@{ "$class\::ISA" }) {
-      return bless $self, $class;
+    if (@{ "$subclass\::ISA" }) {
+      return bless $self, $subclass;
     } else {
-      Carp::croak "invalid URI::cpan: invalid authority '$auth' (from $self)";
+      Carp::croak "invalid $class: invalid authority '$auth' (from $self)";
     }
   }
-}
-
-sub type { 'generic' }
-
-package URI::cpan::_namever;
-use base qw(URI::cpan);
-
-sub NAME    () { 1 }
-sub VERSION () { 2 }
-
-sub name {
-  my ($self, $new) = @_;
-  return +(split m{/}, $self->path)[NAME] if @_ == 1;
-  my $path = join '/', '', $new, grep { defined } $self->version;
-  $self->path($path);
-  return $self;
-}
-
-sub version {
-  my ($self, $new) = @_;
-  return +(split m{/}, $self->path)[VERSION] if @_ == 1;
-  $self->path(join '/', '', $self->name, $new);
-  return $self;
 }
 
 sub authority {
@@ -57,13 +32,86 @@ sub authority {
   Carp::croak "invalid type: must be '$ok'" unless $new eq $ok;
   return $self->SUPER::authority;
 }
+
 BEGIN { *type = \&authority }
+
+sub _path_parts {
+  die "override me";
+}
+
+sub _parse_parts {
+  my $self = shift;
+  return map { 
+    my ($name, $length) = /^(\w+)(?:\[(\d+|\*)\])?$/;
+    $length ||= 1;
+    [ $name => $length ]
+  } @_;
+}
+
+sub _splice_part {
+  my ($self, $part, $new) = @_;
+  my @parts = $self->_parse_parts($self->_path_parts);
+  Carp::croak "invalid path part: '$part'"
+    unless grep { $_->[0] eq $part } @parts;
+
+  my (undef, @path) = split m{/}, $self->path;
+  my $start = 0;
+  for (@parts) {
+    my ($name, $length) = @$_;
+    $length = @path - $start if $length eq '*';
+    if ($name ne $part) {
+      $start += $length;
+      if ($start > $#path) {
+        if ($new) {
+          push @path, @$new;
+          $self->path(join '/', @path);
+          return @$new;
+        } else {
+          return ();
+        }
+      }
+      next;
+    }
+    if ($new) {
+      splice(@path, $start, $length, @$new);
+      $self->path(join '/', @path);
+      return @$new;
+    } else {
+      return splice(@path, $start, $length);
+    }
+  }
+  return ();
+}
+
+sub _path_part {
+  my $self = shift;
+  my $part = shift;
+  my @p;
+  if (@_) {
+    (my $new = shift) =~ s{^/+}{};
+    @p = $self->_splice_part($part => [split m{/}, $new]);
+  } else {
+    @p = $self->_splice_part($part);
+  }
+  my ($length) = map { $_->[1] } grep { $_->[0] eq $part }
+    $self->_parse_parts($self->_path_parts);
+  return join '/', ($length eq 1 ? () : ''), @p;
+}
+
+package URI::cpan::_namever;
+use base qw(URI::cpan);
+
+sub _path_parts { qw(name version) }
+
+sub name    { shift->_path_part(name    => @_) }
+sub version { shift->_path_part(version => @_) }
 
 sub vname {
   my ($self) = @_;
+  my $version = $self->version;
   Carp::croak "cannot produce vname for url without version: $self"
-    unless defined $self->version;
-  return join $self->sep, (split m{/}, $self->path)[NAME, VERSION];
+    unless defined $version;
+  return join $self->sep, $self->name, $version;
 }
 
 sub _sep { die }
@@ -76,27 +124,16 @@ package URI::cpan::package;
 use base qw(URI::cpan::_namever);
 sub sep { ' ' }
 
-package URI::cpan::author;
+package URI::cpan::id;
 use base qw(URI::cpan);
-use File::Basename ();
 use URI::Escape ();
 
-sub type { 'author' }
-
-sub authority { 
-  my $self = shift;
-  return $self->SUPER::authority unless @_;
-  my $new = shift;
-  Carp::croak "invalid cpanid: must be all uppercase letters (got '$new')"
-    unless $new =~ /^[A-Z]+$/;
-  return $self->SUPER::authority($new);
+sub _path_parts {
+  return qw(cpanid file_path[*]);
 }
-BEGIN { *cpanid = \&authority }
 
-sub filename {
-  my ($self) = @_;
-  return File::Basename::basename($self->path);
-}
+sub cpanid    { shift->_path_part(cpanid    => @_) }
+sub file_path { Path::Class::file(shift->_path_part(file_path => @_)) } 
 
 sub full_path     { shift->_full_path }
 sub full_path_url { shift->_full_path(1) }
@@ -110,7 +147,7 @@ sub _full_path {
       substr($id, 0, 1),
       substr($id, 0, 2),
       $id,
-      grep { defined && length } split m{/}, $self->path,
+      grep { defined && length } split m{/}, $self->file_path,
   ;
 }
 
