43dbb5ad9ebbd32fae397b25552c566876a20741
Hans Dieter Pearcey <hdp@pobox.com>
2008-03-27 11:46:23 -0400

    xpan://

---
+++ lib/URI/xpan.pm
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+
+use URI::cpan;
+
+package URI::xpan;
+our @ISA = qw(URI::cpan);
+
+package URI::xpan::dist;
+our @ISA = qw(URI::cpan::dist);
+
+package URI::xpan::package;
+our @ISA = qw(URI::cpan::package);
+
+package URI::xpan::id;
+our @ISA = qw(URI::cpan::id);
+
+1;
