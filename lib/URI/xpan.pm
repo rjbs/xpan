8eff008a8b91cb4730d43718b72fc1163cdeca78
Hans Dieter Pearcey <hdp@pobox.com>
2008-03-27 14:28:20 -0400

    add pin:// urls

--- lib/URI/xpan.pm
+++ lib/URI/xpan.pm
@@ -9,10 +9,15 @@ our @ISA = qw(URI::cpan);
 package URI::xpan::dist;
 our @ISA = qw(URI::cpan::dist);
 
+package URI::xpan::pin;
+our @ISA = qw(URI::xpan::dist);
+
 package URI::xpan::package;
 our @ISA = qw(URI::cpan::package);
 
 package URI::xpan::id;
 our @ISA = qw(URI::cpan::id);
+
+sub id { shift->cpanid(@_) }
 
 1;
