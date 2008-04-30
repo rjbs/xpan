212772ec613ad754ac9764e268d4159b26fd1831
Hans Dieter Pearcey <hdp@pobox.com>
2008-04-30 14:06:16 -0400

    allow checking out a ref

--- lib/XPAN/Injector/Git.pm
+++ lib/XPAN/Injector/Git.pm
@@ -8,9 +8,25 @@ with qw(XPAN::Injector::VCS);
 
 sub scheme { 'git' }
 
+my %CHECKOUT = (
+  tag => sub { $_[0] },
+);
+
 sub export_to_dir {
   my ($self, $url, $dir) = @_;
+  my $path = $url->path;
+  $url->path('') if $path;
   system("git clone $url $dir") and die "git clone $url $dir failed: $?";
+  if ($path) {
+    $path =~ s{^/+}{};
+    my ($type, $val) = split m{/}, $path, 2;
+
+    die "invalid path: $path" unless $CHECKOUT{$type};
+
+    my $cmd = "git checkout " . $CHECKOUT{$type}->($val);
+    system($cmd) and die "$cmd failed: $?";
+  }
+
   Path::Class::dir($dir)->subdir('.git')->rmtree;
 }
 
